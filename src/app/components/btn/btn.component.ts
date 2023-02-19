e8ad6878c323388e8bbb373e4b9b1f840e4b013c
Mauricio Godoy <95603787+Mauri-Godoy@users.noreply.github.com>
2023-02-18 21:16:35 -0300

    colores y propiedades dinámicas

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -8,9 +8,19 @@ export class BtnComponent implements OnInit {
 
   @Input() type: 'button' | 'reset' | 'submit' = 'button';
 
+  // Tailwind no puede leer clases dinámicas
+  @Input() color = 'primary';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public colorVariants: Map<any, string> = new Map(
+    [
+      ['success', 'bg-success-600 hover:bg-success-500 focus:ring-success-300'],
+      ['primary', ' bg-primary-600 hover:bg-primary-500 focus:ring-primary-300'],
+      ['red', ' bg-red-600 hover:bg-red-500 focus:ring-red-300']
+    ]
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: { files: ["./src/**/*.{html,js,ts}"], relative: true, },
   theme: {
     extend: {
       colors: {
