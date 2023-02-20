73b1b04c628861337041f478086d652d83dfaea8
Mauricio Godoy <95603787+Mauri-Godoy@users.noreply.github.com>
2023-02-20 20:23:44 -0300

    FontAwesome en el proyecto
    $ ng add @fortawesome/angular-fontawesome

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faBell, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html'
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   isOpen = false;
+
+  faBell = faBell;
+  faInfoCircle = faInfoCircle;
 
   constructor() { }
 
