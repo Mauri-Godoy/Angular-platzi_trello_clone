68bd13a06f9ef006729612fd38eb8cc6c01cee87
Mauricio Godoy <95603787+Mauri-Godoy@users.noreply.github.com>
2023-02-20 20:44:57 -0300

    Sobreescribiendo utilidad container de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = {
         //con esto le ponemos un alias
         success: colors.lime,
         primary: colors.sky
+      },
+      // con tailwind.config se puede sobreescribir una una funcionalidad
+      container: {
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1024px',
+          '2xl': '1536px',
+        }
       }
     },
   },
